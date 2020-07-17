cc5b3725ef386addc81490a27a9edf973d733530
Victor Shchelkunov <victor.shchelkunov@devexpress.com>
2020-07-17 17:09:28 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # devextreme-themebuilder-service
-A web service that provides access to the devextreme-themebuilder API
+A web service that provides access to the [devextreme-themebuilder](https://www.npmjs.com/package/devextreme-themebuilder) API.
+
+## Prerequisites
+
+You will need Node.js 8.9 or higher and npm 5.5.1 or higher.
+
+## Install required packages and Launch
+
+Run the following commands:
+ 
+```
+npm install
+npm run start
+```
+
+Go to http://localhost:3000/.
---
+++ index.js
@@ -0,0 +1,24 @@
+const builder = require("devextreme-themebuilder/modules/builder");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const compression = require("compression");
+const express = require("express");
+const app = express();
+
+app.use(cors());
+app.use(compression());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.post('/buildtheme', function (req, res) {
+    builder.buildTheme(req.body)
+        .then((result) => {
+            res.json(result);
+        }, (err) => {
+            res.status(500).send(err);
+        })
+});
+
+app.listen(3000);
