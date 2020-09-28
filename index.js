2297ccbff0e672b7b05f19c8dd047cc72903b128
Oleg Kipchatov <35765711+OlegKipchatov@users.noreply.github.com>
2020-09-28 19:51:46 +0300

    Support metadata request (#1)

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 "use strict";
 
 const builder = require("devextreme-themebuilder/modules/builder");
+const metadata = require("devextreme-themebuilder/data/metadata/dx-theme-builder-metadata").metadata;
+const baseParameters = require("devextreme-themebuilder/modules/base-parameters");
+const themes = require("devextreme-themebuilder/modules/themes").default;
+const version = require("devextreme-themebuilder/package.json").version;
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const compression = require("compression");
@@ -13,6 +17,32 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+
+function createMetadata() {
+    const result = metadata;
+
+    if(themes) {
+        result.themes = themes;
+    }
+
+    if(version) {
+        result.version = version
+    }
+
+    if(baseParameters) {
+        result.baseParameters = baseParameters;
+    }
+
+    return result;
+}
+
+const currentMetadata = createMetadata();
+
+app.get('/metadata', (req, res) => {
+    if(!currentMetadata) res.status(500).send({ err: 'No metadata' });
+
+    res.status(200).send(currentMetadata);
+});
 
 app.post('/buildtheme', (req, res) => {
     builder.buildTheme(req.body)
